e9833ea82cd2db54a24c896129b9ea031f2825fa
paulakedouk <paulakedouk@gmail.com>
2019-12-13 10:11:36 -0800

    fix the venues id page

--- config.py
+++ config.py
@@ -8,6 +8,5 @@ DEBUG = True
 
 # Connect to the database
 
-
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgres://paulakedouk:012300a@localhost:5432/fyyur'
